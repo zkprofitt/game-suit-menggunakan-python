5faaa4ccaee7e3c2b01dc9424d5b0fa720a83362
zkcodee <zaki.dafa15@gmail.com>
2026-02-11 11:53:23 +0700

    melakukan penambahan pada kondisi kemungkinan dan break pada while nya

--- AduSuit.py
+++ AduSuit.py
@@ -11,3 +11,27 @@ while True:
     pilihan_mesin = random.choice(kemungkinan)
     print (f" \n pilihan kamu {pilihan_pemain}, dan pilihan mesin {pilihan_mesin} \n ")
 
+    if pilihan_pemain == pilihan_mesin:
+        print(f" Kedua Pemain sama-sama memilih {pilihan_pemain}. kalian seri💪")
+    elif pilihan_pemain == "gunting":
+        if pilihan_mesin == "kertas":
+            print(" gunting mengalahkan kertas, kamu menang!😁")
+        else:
+            print(" batu mengalahkan gunting, kamu kalah!😅")
+    elif pilihan_pemain == "batu":
+        if pilihan_mesin == "gunting":
+            print(" batu mengalahkan gunting, kamu menang!😁")
+        else:
+            print(" kertas mengalahkan batu, kamu kalah!😅")
+    elif pilihan_pemain == "kertas":
+        if pilihan_mesin == "batu":
+            print(" kertas mengalahkan batu, kamu menang!😁")
+        else:
+            print(" gunting mengalahkan kertas, kamu kalah!😅")
+
+    main_lagi = input(" \n mau main lagi? (y/n):")
+    if main_lagi.lower() != "y":
+        print("\n terimakasih sudah bermain. kapan kapan coba lagi ya 👋👋👋")
+        break
+
+    
