d2730e266bfd86de3263c34819c03bb477a02ea6
zkcodee <zaki.dafa15@gmail.com>
2026-02-27 15:05:52 +0700

    menambahkan warna pada output di terminal agar lebih menarik

--- AduSuit.py
+++ AduSuit.py
@@ -1,4 +1,7 @@
 import random
+import colorama
+from colorama import Fore, Style
+colorama.init(autoreset= True)
 
 print ("\n ==================================================================")
 print (" Selamat Datang Pada Game Gunting Batu Kertas 👋👋")
@@ -14,29 +17,29 @@ while True:
     print (f" \n pilihan kamu {pilihan_pemain}, dan pilihan mesin {pilihan_mesin} \n ")
 
     if pilihan_pemain == pilihan_mesin:
-        print(f" Kedua Pemain sama-sama memilih {pilihan_pemain}. kalian seri💪")
+        print(Fore.LIGHTMAGENTA_EX + f" Kedua Pemain sama-sama memilih {pilihan_pemain}. kalian seri💪")
         skor_mesin += 1
         skor_pemain += 1
     elif pilihan_pemain == "gunting":
         if pilihan_mesin == "kertas":
-            print(" gunting mengalahkan kertas, kamu menang!😁")
+            print(Fore.GREEN + " gunting mengalahkan kertas, kamu menang!😁")
             skor_pemain += 1
         else:
-            print(" batu mengalahkan gunting, kamu kalah!😅")
+            print(Fore.RED + " batu mengalahkan gunting, kamu kalah!😅")
             skor_mesin += 1
     elif pilihan_pemain == "batu":
         if pilihan_mesin == "gunting":
-            print(" batu mengalahkan gunting, kamu menang!😁")
+            print(Fore.GREEN + " batu mengalahkan gunting, kamu menang!😁")
             skor_pemain += 1
         else:
-            print(" kertas mengalahkan batu, kamu kalah!😅")
+            print(Fore.RED +" kertas mengalahkan batu, kamu kalah!😅")
             skor_mesin += 1
     elif pilihan_pemain == "kertas":
         if pilihan_mesin == "batu":
-            print(" kertas mengalahkan batu, kamu menang!😁")
+            print(Fore.GREEN + " kertas mengalahkan batu, kamu menang!😁")
             skor_pemain += 1
         else:
-            print(" gunting mengalahkan kertas, kamu kalah!😅")
+            print(Fore.RED + " gunting mengalahkan kertas, kamu kalah!😅")
             skor_mesin += 1
 
     main_lagi = input(" \n mau main lagi? (y/n):")
@@ -44,9 +47,12 @@ while True:
         print("\n terimakasih sudah bermain. kapan kapan coba lagi ya 👋👋👋")
         print(f"\n skor kamu : {skor_pemain} skor mesin : {skor_mesin}")
         if skor_pemain > skor_mesin:
-            print("\n selamat kamu menang!")
+            print(Fore.GREEN + "\n selamat kamu menang!")
+        elif skor_pemain == skor_mesin:  
+            print(Fore.LIGHTMAGENTA_EX + "\n skor kamu sama nih kayak mesinnya 😅")
+          
         else:
-            print("\n yah kamu kalah, jangan patah semangat yuk coba lagi! ")
+            print(Fore.RED + "\n yah kamu kalah, jangan patah semangat yuk coba lagi! 💪💪")
         break
 
     
